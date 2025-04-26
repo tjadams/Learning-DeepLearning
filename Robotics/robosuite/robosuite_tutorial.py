1c6a58da1178a34d305a7fd4eb0c3be470f8e3f2
Tyler Adams <tjadams.giga@gmail.com>
2025-04-26 16:45:24 -0400

    Limit frame rate

--- Robotics/robosuite/robosuite_tutorial.py
+++ Robotics/robosuite/robosuite_tutorial.py
@@ -1,5 +1,6 @@
 import numpy as np
 import robosuite as suite
+import time
 
 env = suite.make(env_name="Lift",
                  robots="Jaco",
@@ -9,7 +10,16 @@ env = suite.make(env_name="Lift",
 
 env.reset()
 
+MAX_FR = 25  # max frame rate for running simluation
+
 for i in range(1000):
   action = np.random.randn(*env.action_spec[0].shape) * 0.1
   obs, reward, done, info = env.step(action)
   env.render()
+
+  start = time.time()
+  # limit frame rate if necessary
+  elapsed = time.time() - start
+  diff = 1 / MAX_FR - elapsed
+  if diff > 0:
+      time.sleep(diff)
