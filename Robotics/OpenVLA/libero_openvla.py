41562adbff921dc9352431ac416d98f48bd9e275
Tyler Adams <tjadams.giga@gmail.com>
2025-06-07 17:52:09 -0400

    Normalize and invert gripper to get OpenVLA to fully work, and add success state + stopwatch

--- Robotics/OpenVLA/libero_openvla.py
+++ Robotics/OpenVLA/libero_openvla.py
@@ -1,7 +1,9 @@
 import os
+import time
 from libero.libero import benchmark, get_libero_path
 from libero.libero.envs import OffScreenRenderEnv
 import imageio
+import numpy as np
 
 # import sys
 # sys.path.append('../')
@@ -63,19 +65,67 @@ prompt = "pick up the alphabet soup and place it in the basket"
 unnorm_key = task_suite_name
 ###
 
+def normalize_gripper_action(action, binarize=True):
+    """
+    Changes gripper action (last dimension of action vector) from [0,1] to [-1,+1].
+    Necessary for some environments (not Bridge) because the dataset wrapper standardizes gripper actions to [0,1].
+    Note that unlike the other action dimensions, the gripper action is not normalized to [-1,+1] by default by
+    the dataset wrapper.
+
+    Normalization formula: y = 2 * (x - orig_low) / (orig_high - orig_low) - 1
+    """
+    # Just normalize the last action to [-1,+1].
+    orig_low, orig_high = 0.0, 1.0
+    action[..., -1] = 2 * (action[..., -1] - orig_low) / (orig_high - orig_low) - 1
+
+    if binarize:
+        # Binarize to -1 or +1.
+        action[..., -1] = np.sign(action[..., -1])
+
+    return action
+
+def invert_gripper_action(action):
+    """
+    Flips the sign of the gripper action (last dimension of action vector).
+    This is necessary for some environments where -1 = open, +1 = close, since
+    the RLDS dataloader aligns gripper actions such that 0 = close, 1 = open.
+    """
+    action[..., -1] = action[..., -1] * -1.0
+    return action
+
+print("Starting to perform the Libero task!")
+start_time = time.time()
 replay_images = []
-for step in range(100):
+# TODO: is there some way to watch the simulation as it's happening, 
+# rather than waiting for the video to be made?
+for step in range(200):
+    # TODO: add a delay here (dummy actions) due to the env
+    # initialization causing objects to drop to the floor for a few seconds
+    
     libero_img = get_libero_image(obs)
     
     observation, libero_img = model.process_libero_observation(libero_img, obs)
             
     action = model.get_action(observation, prompt, unnorm_key)
     
+    # TODO: understand more about why these 3 steps are needed
+    # Normalize and apply action
+    action = normalize_gripper_action(action, binarize=True)
+    action = invert_gripper_action(action)
+    action = action.tolist()
+    
     obs, reward, done, info = env.step(action)
     
     print(f"Processed action! Step #: {step}")
     
+    elapsed_time = time.time() - start_time
+    print(f"Elapsed time: {elapsed_time:.4f} seconds")
+    
     replay_images.append(libero_img)
+    
+    if done:
+        print(f"Episode completed successfully in {step} steps!")
+        break
 env.close()
 
 video_dir = "outputs/videos"
