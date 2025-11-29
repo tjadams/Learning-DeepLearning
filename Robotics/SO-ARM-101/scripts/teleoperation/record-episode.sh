2ecb20f1703ce4b2985851d05039c0132c958a89
Tyler Adams <tjadams.giga@gmail.com>
2025-11-29 00:59:24 -0500

    Recording path

--- Robotics/SO-ARM-101/scripts/teleoperation/record-episode.sh
+++ Robotics/SO-ARM-101/scripts/teleoperation/record-episode.sh
@@ -1,4 +1,5 @@
 # Usage for dataset.repo_id: my HF_USER is the same name as my mac username so it causes some error. So I just use a different folder name. However, I had to change the folder name for each recording due to another error.
+# Recordings are stored locally in ~/.cache/huggingface/lerobot/<dataset.repo_id>
 
 # Inspired by https://huggingface.co/docs/lerobot/en/getting_started_real_world_robot
 # Camera value from camera-init.sh, ports from find-robot-ports.sh
